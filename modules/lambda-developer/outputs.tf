dd165793ef01573d031f9323dbcd666a70498d3a
kmccanless <keith@testdouble.com>
2022-07-05 13:07:43 -0400

    hard coded index

--- modules/lambda-developer/outputs.tf
+++ modules/lambda-developer/outputs.tf
@@ -5,5 +5,5 @@ output "lambda_arn" {
   value = aws_lambda_function.lambda_function.arn
 }
 output "api_gateway_endpoint" {
-   value = aws_apigatewayv2_api.lambda-gw[count.index].api_endpoint
+   value = aws_apigatewayv2_api.lambda-gw[0].api_endpoint
 }
