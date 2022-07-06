41507363c35c7be3f5098b56d51ea16ca26a270b
kmccanless <keith@testdouble.com>
2022-07-06 14:13:39 -0400

    added api gw output

--- modules/lambda-developer/outputs.tf
+++ modules/lambda-developer/outputs.tf
@@ -7,3 +7,9 @@ output "lambda_arn" {
 output "api_gateway_endpoint" {
    value = aws_apigatewayv2_api.lambda-gw[0].api_endpoint
 }
+output "table_arn" {
+  value = aws_dynamodb_table.dynamodb-table.arn
+}
+output "table_id" {
+  value = aws_dynamodb_table.dynamodb-table.id
+}
